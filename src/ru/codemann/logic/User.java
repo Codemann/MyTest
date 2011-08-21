7df897b119e3292f471ffae478538abb7cf0fe98
Сodemann <codemann.is@gmail.com>
2011-08-21 14:12:39 +0400

    Добавил в класс User конструктор и изменил название поняля name на id

--- src/ru/codemann/logic/User.java
+++ src/ru/codemann/logic/User.java
@@ -2,18 +2,24 @@ package ru.codemann.logic;
 
 public class User {
 
-	private String name;
+	private String id;
 	private String login;
 	private String passHash;
 	
-	public String getName() {
-		return name;
+	public User(String id, String login, String passHash) {
+		this.id = id;
+		this.login = login;
+		this.passHash = passHash;
 	}
-	
-	public void setName(String name) {
-		this.name = name;
+
+	public String getId() {
+		return id;
 	}
-	
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getLogin() {
 		return login;
 	}
